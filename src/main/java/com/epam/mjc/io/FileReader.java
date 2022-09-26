97b166507d147c00eae7fbdcfe2ae2af95a41e1e
Shushanna <shushannagrigoryan7@gmail.com>
2022-09-26 16:29:48 +0400

    1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String str = "";
+        try(FileInputStream stream = new FileInputStream("Profile.txt")){
+            int symbol;
+            while ((symbol = stream.read()) != -1) {
+                str += (char)symbol;
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error while reading the file.");
+        }
+
+        Map<String, String> m = new HashMap<>();
+        String [] pairs = str.split("\n");
+        for(String item:pairs){
+            String [] keyValue = item.split(": ");
+            m.put(keyValue[0], keyValue[1]);
+        }
+        String name = m.get("Name");
+        int age = Integer.parseInt(m.get("Age"));
+        String email = m.get("Email");
+        long phone = Long.parseLong(m.get("Phone"));
+
+
+        return new Profile(name, age, email, phone);
     }
 }
